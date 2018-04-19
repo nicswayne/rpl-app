8fbbb89732e660bc8d80fd6ab04cdfb76ee5ff5e
Nicodemus Swayne <nic@dialexa.com>
2018-04-19 15:15:36 -0500

    Basic setup.

---
+++ README.md
@@ -0,0 +1,1 @@
+read play love
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+import App from './components/App';
+
+import './index.css';
+
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
+);
+
+registerServiceWorker();
